7b3e54975875cd90c20aa0f7cb352d475a8f0b61
prograH <pruebas.progras@gmail.com>
2017-07-02 09:16:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,9 @@
   <title>Prueba de PHP</title>
  </head>
  <body bgcolor="gray">
-  <form action="" method="post">
- <p>Su nombre: <input type="text" name="nombre" /></p>
- <p>Su edad: <input type="text" name="edad" /></p>
- <p><input type="submit" /></p>
-</form>
  <?php 
    echo '<p>Hola Mundo</p>';
+   echo '<p>Hola Mundo3</p>';
    ?>
  </body>
 </html>
