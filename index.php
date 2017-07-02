574a140d416c6d106415e186e3967fa411f5bd2d
prograH <pruebas.progras@gmail.com>
2017-07-02 00:43:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
-echo '<p>Hola Mundo</p>';
-?>
-
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body bgcolor="gray">
+ <?php 
+   echo '<p>Hola Mundo</p>';
+   ?>
+ </body>
+</html>
